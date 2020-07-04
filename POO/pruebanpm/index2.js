f92ba07063196e1fccb903b8f22cf292ad95273e
FedericoAr <federicoarambillet37@gmail.com>
2020-07-04 19:00:06 -0300

    Ejercicio Clase 03.07.2020

--- POO/pruebanpm/index2.js
+++ POO/pruebanpm/index2.js
@@ -5,6 +5,7 @@ console.log('i like cake and pies'.underline.red) // outputs red underlined text
 console.log('inverse the color'.inverse); // inverses the color
 console.log('OMG Rainbows!'.rainbow); // rainbow
 console.log('Run the trap'.trap); // Drops the bass
+ 
 
 
 var colors = require('colors/safe');

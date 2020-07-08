d8376ca151611fd9b93a4f00d60f2987180d604c
FedericoAr <federicoarambillet37@gmail.com>
2020-07-08 15:18:26 -0300

    Se arregla indentación en archivo ejemplo.ts

--- POO/npm-Clase03.07.2020/ejemplo.ts
+++ POO/npm-Clase03.07.2020/ejemplo.ts
@@ -1,6 +1,7 @@
 function ejemplo(nombre: string): string {
     return "Hola " + nombre;
-    }
-    let nombre: string;
-    nombre = 'Juan';
-    console.log(ejemplo(nombre));
+}
+
+let nombre: string;
+nombre = 'Juan';
+console.log(ejemplo(nombre));

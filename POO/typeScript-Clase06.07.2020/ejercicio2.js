bddc5b93c3ad1285fcb755ee234271f905c574a8
FedericoAr <federicoarambillet37@gmail.com>
2020-07-09 17:05:47 -0300

    Se modifica la función actualizar y eliminar

--- POO/typeScript-Clase06.07.2020/ejercicio2.js
+++ POO/typeScript-Clase06.07.2020/ejercicio2.js
@@ -4,61 +4,65 @@ exports.__esModule = true;
 var readlineSync = require("readline-sync");
 //Declaración e inicialización de variables
 var long;
-var palabras = new Array(long);
 var i;
 var sistema;
 //Función para cargar arreglo
-var cargarArreglo = function (arreglo, longitud) {
-    var aux = longitud;
-    var input;
-    for (i = 0; i < longitud; i++) {
+var cargarArreglo = function (arreglo) {
+    for (i = 0; i < arreglo.length; i++) {
         arreglo[i] = readlineSync.question("Ingrese una palabra: ");
     }
     console.log("Registrado con exito!");
+    console.log(arreglo);
 };
 //Función para buscar en arreglo
 var buscarArreglo = function (arreglo) {
-    var nombre = readlineSync.question("Ingrese la palabra que desea buscar: ");
+    var nombre = readlineSync.question("Ingrese la palabra: ");
     var dato;
     for (i = 0; i < arreglo.length; i++) {
         dato = arreglo[i];
-    }
-    if (dato == nombre) {
-        console.log("La palabra ingresada se encuentra registrado en la posicion " + i + ".");
-    }
-    else {
-        console.log("La palabra ingresada no se encuentra registrada.");
+        if (dato == nombre) {
+            return i;
+        }
     }
 };
 //Función para actualizar en arreglo
 var actualizarPalabra = function (arreglo) {
-    console.log("Datos que se encuentran actualmente cargados", arreglo);
-    arreglo[readlineSync.questionInt("¿Ingresa la posicion del elemento que deseas actualizar? ")] = readlineSync.question("Ingresa una nueva palabra: ");
-    console.log("Así quedaron los datos despues de la actualización", arreglo);
+    console.log(arreglo);
+    var index = buscarArreglo(arreglo);
+    arreglo[index] = readlineSync.question("Ingresa una nueva palabra: ");
+    console.log(arreglo);
 };
 //Función para eliminar dato
 var eliminarIndex = function (arreglo) {
-    console.log("Datos que se encuentran actualmente cargados", arreglo);
-    var pos = readlineSync.questionInt("¿Ingresa la posicion del elemento que deseas eliminar? ");
-    for (i = pos; i < arreglo.length - 1; i++) {
+    console.log("Datos que se encuentran actualmente cargados antes de la eliminación", arreglo);
+    var index = buscarArreglo(arreglo);
+    for (i = index; i < arreglo.length - 1; i++) {
         arreglo[i] = arreglo[i + 1];
     }
     arreglo[i] = null;
-    console.log("Así quedaron los datos despues de la eliminacion", arreglo);
+    console.log("Así quedaron los datos despues de la eliminación", arreglo);
 };
 console.log("<----------BIENVENIDO---------->");
 console.log("OPCIONES:");
-console.log("(1) Insertar palabra \n(2) Insertar buscar \n(3) Eliminar palabra \n(4) Actualizar palabra\n(5) Salir");
+console.log("(1) Insertar palabra \n(2) Buscar palabra \n(3) Eliminar palabra \n(4) Actualizar palabra \n(5) Salir");
 sistema = readlineSync.questionInt("¿Que desea hacer? ");
 long = readlineSync.questionInt("Ingrese la longitud del arreglo: ");
+var palabras = new Array(long);
+//Menu
 while (sistema != 5) {
     switch (sistema) {
         case 1: {
-            cargarArreglo(palabras, long);
+            cargarArreglo(palabras);
             break;
         }
         case 2: {
-            buscarArreglo(palabras);
+            var resultado = buscarArreglo(palabras);
+            if (resultado >= 0) {
+                console.log("La palabra ingresada se encuentra registrada en la posicion " + resultado + ".");
+            }
+            else {
+                console.log("La palabra ingresada no se encuentra registrada.");
+            }
             break;
         }
         case 3: {
--- POO/typeScript-Clase06.07.2020/ejercicio2.ts
+++ POO/typeScript-Clase06.07.2020/ejercicio2.ts
@@ -3,59 +3,59 @@ import * as readlineSync from "readline-sync";
 
 //Declaración e inicialización de variables
 let long: number;
-let palabras = new Array(long);
 let i: number;
 let sistema: number;
 
 //Función para cargar arreglo
-let cargarArreglo = (arreglo: string[], longitud: number) => {
-    let aux: number = longitud;
-    let input: string;
-    for (i = 0; i < longitud; i++) {
+let cargarArreglo = (arreglo: string[]) => {
+    for (i = 0; i < arreglo.length; i++) {
         arreglo[i] = readlineSync.question("Ingrese una palabra: ");
 
     }
     console.log("Registrado con exito!");
+    console.log(arreglo);
 }
 
 //Función para buscar en arreglo
-let buscarArreglo = (arreglo: string[]) => {
-    let nombre: string = readlineSync.question("Ingrese la palabra que desea buscar: ");
+let buscarArreglo = (arreglo: string[]): number => {
+    let nombre: string = readlineSync.question("Ingrese la palabra: ");
     let dato: string;
     for (i = 0; i < arreglo.length; i++) {
         dato = arreglo[i];
-    }
-    if (dato == nombre) {
-        console.log(`La palabra ingresada se encuentra registrado en la posicion ${i}.`);
-    } else {
-        console.log("La palabra ingresada no se encuentra registrada.");
+        if (dato == nombre) {
+            return i;
+        }
     }
 }
 
 //Función para actualizar en arreglo
 let actualizarPalabra = (arreglo: string[]) => {
-    console.log("Datos que se encuentran actualmente cargados", arreglo);
-    arreglo[readlineSync.questionInt("¿Ingresa la posicion del elemento que deseas actualizar? ")] = readlineSync.question("Ingresa una nueva palabra: ");
-    console.log("Así quedaron los datos despues de la actualización", arreglo);
+    console.log(arreglo);
+    let index: number = buscarArreglo(arreglo);
+    arreglo[index] = readlineSync.question("Ingresa una nueva palabra: ");
+    console.log(arreglo);
 }
 
 //Función para eliminar dato
 let eliminarIndex = (arreglo: string[]) => {
-    console.log("Datos que se encuentran actualmente cargados", arreglo);
-    let pos: number = readlineSync.questionInt("¿Ingresa la posicion del elemento que deseas eliminar? ");
-    for (i = pos; i < arreglo.length - 1; i++) {
+    console.log("Datos que se encuentran actualmente cargados antes de la eliminación", arreglo);
+    let index: number = buscarArreglo(arreglo);
+    for (i = index; i < arreglo.length - 1; i++) {
         arreglo[i] = arreglo[i + 1];
     }
     arreglo[i] = null;
-    console.log("Así quedaron los datos despues de la eliminacion", arreglo);
+    console.log("Así quedaron los datos despues de la eliminación", arreglo);
 }
 
 
 console.log("<----------BIENVENIDO---------->")
 console.log("OPCIONES:");
-console.log("(1) Insertar palabra \n(2) Insertar buscar \n(3) Eliminar palabra \n(4) Actualizar palabra\n(5) Salir");
+console.log("(1) Insertar palabra \n(2) Buscar palabra \n(3) Eliminar palabra \n(4) Actualizar palabra \n(5) Salir");
 sistema = readlineSync.questionInt("¿Que desea hacer? ");
+
+//Se le pide al usuario que ingrese la longitud y se crea un arreglo con esa longitud
 long = readlineSync.questionInt("Ingrese la longitud del arreglo: ");
+let palabras = new Array(long);
 
 //Menu
 while (sistema != 5) {
@@ -63,11 +63,17 @@ while (sistema != 5) {
     switch (sistema) {
 
         case 1: {
-            cargarArreglo(palabras, long);
+            cargarArreglo(palabras);
             break;
         }
         case 2: {
-            buscarArreglo(palabras);
+            let resultado: number = buscarArreglo(palabras);
+            if(resultado >= 0){
+                console.log(`La palabra ingresada se encuentra registrada en la posicion ${resultado}.`);
+            }else{
+                console.log(`La palabra ingresada no se encuentra registrada.`);
+
+            }
             break;
         }
         case 3: {
